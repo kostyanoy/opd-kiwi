b4c2b3639675f0556dde3f5fe1ead2f5fa52901f
Kosty Manuhin <kostymanuhin@gmail.com>
2023-03-25 00:56:10 +0300

    experts page beautify

--- php/experts.php
+++ php/experts.php
@@ -3,22 +3,32 @@
 
 <head>
     <title>Эксперты</title>
+    <link rel="stylesheet" href="/css/experts.css">
 </head>
 
 <body>
-    Наши эксперты: 
-    <?php
-    include_once("boot.php");
-    $conn = get_db_connection();
+    <h1>Наши эксперты</h1>
+    <div class="container">
+        <div class="experts">
+            <?php
+            include_once("boot.php");
+            $conn = get_db_connection();
 
-    $experts = $conn->query("SELECT * FROM users WHERE status='expert'");
-    $i = 1;
+            $experts = $conn->query("SELECT * FROM users WHERE status='expert'");
 
-    while ($result = $experts->fetch_array()) {
-        echo "<p>{$i}) {$result['login']} - {$result['age']} - {$result['gender']}</p>";
-        $i++;
-    }
-    ?>
+            while ($result = $experts->fetch_array()) {
+                echo "<div class='expert-card'>";
+                echo "<div class='expert-image'><img src='/images/profile-picture.png' alt='profile'></div>";
+                echo "<div class='expert-info'>";
+                echo "<h2>{$result['login']}</h2>";
+                echo "<p>{$result['age']} - {$result['gender']}</p>";
+                echo "</div>";
+                echo "</div>";
+            }
+            ?>
+        </div>
+        <a href="/index.html" class="back-button">Назад</a>
+    </div>
 </body>
 
 </html>
