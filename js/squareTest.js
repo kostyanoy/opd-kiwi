548637014f567b35b194a9a434e36a5cda5d2889
ferea <tatamitina19@gmail.com>
2023-03-28 18:17:05 +0300

    вернула обратно кусок кода(((

--- js/squareTest.js
+++ js/squareTest.js
@@ -1,50 +1,66 @@
 const getRandomInt = (min, max) => {
 	min = Math.ceil(min);
 	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+//средне квадратичное отклонение
+const sampleStandartDeviation = (numbers) => {
+	const avg = numbers.reduce((acc, curr) => acc + curr, 0) / numbers.length;
+	const s = numbers.reduce((acc, curr) => acc + (avg - curr) ** 2, 0);
+	return Math.sqrt(s / (numbers.length - 1));
+}
+
+const isActive = (elements) => {
+	for (let i = 0; i < elements.length; i++) {
+		if (elements[i] === document.activeElement) {
+			return true;
+		}
+	}
+	return false;
 }
 
 class Result {
 	constructor(min, max, message) {
-		this.min = min;
-		this.max = max;
-		this.message = message;
+		this.min = min
+		this.max = max
+		this.message = message
 	}
 
 	checkValue(value) {
-		return value >= this.min && value < this.max;
+		return value >= this.min && value < this.max
 	}
 }
 
 class TestContainer {
 	constructor(squares, startButton, resultLabel, baseColor, amount) {
-		this.squares = squares;
-		this.startButton = startButton;
-		this.resultLabel = resultLabel;
-		this.baseColor = baseColor;
-		this.amount = amount;
+		this.squares = squares
+		this.startButton = startButton
+		this.resultLabel = resultLabel
+		this.baseColor = baseColor
+		this.amount = amount
 
-		this.reactionTime = [];
-		this.colors = ["red", "yellow", "green"];
+		this.reactionTime = []
+		this.colors = ["red", "yellow", "green"]
 
-		this.reset();
+		this.reset()
 	}
 
 	reset() {
-		this.totalTime = 0;
-		this.curSquare = 0;
-		this.correct = 0;
-		this.incorrect = 0;
-		this.num = 0;
-		this.isActive = false;
-		this.time = performance.now();
-		this.reactionTime = [];
+		this.totalTime = 0
+		this.curSquare = 0
+		this.correct = 0
+		this.incorrect = 0
+		this.num = 0
+		this.isActive = false
+		this.time = performance.now()
+		this.reactionTime = []
 
-		this.resetColors();
+		this.resetColors()
 	}
 
 	setLabel(text) {
-		this.resultLabel.textContent = text; // изменен innerHTML на textContent
+		this.resultLabel.innerHTML = text
 	}
 
 	chooseResult(results, value) {
@@ -56,37 +72,39 @@ class TestContainer {
 	}
 
 	getAverageTime() {
-		return Math.round(this.totalTime / this.num);
+		return Math.round(this.totalTime / this.num)
 	}
 
 	addReactionTime(time) {
-		this.reactionTime.push(time);
-		this.totalTime += time;
+		this.reactionTime.push(time)
+		this.totalTime += time
 	}
+
 
 	//square methods
 	resetColors() {
 		for (let i = 0; i < this.squares.length; i++) {
-			this.changeSquareColor(i, this.baseColor);
+			this.changeSquareColor(i, this.baseColor)
 		}
 	}
 
 	changeCurrentSquareColor(color) {
-		this.changeSquareColor(this.curSquare, color);
+		this.changeSquareColor(this.curSquare, color)
 	}
 
 	changeSquareColor(index, color) {
-		this.squares[index].style.backgroundColor = color;
+		this.squares[index].style.backgroundColor = color
 	}
 
 	checkCurrentSquare() {
-		return this.checkSquare(this.curSquare);
+		return this.checkSquare(this.curSquare)
 	}
 
 	checkSquare(index) {
-		return this.squares[index].style.backgroundColor !== this.baseColor;
+		return this.squares[index].style.backgroundColor !== this.baseColor
 	}
 }
+
 
 class Test1 extends TestContainer {
 	constructor(squares, startButton, resultLabel, baseColor = "lightgrey", amount = 10) {
@@ -165,23 +183,194 @@ class Test1 extends TestContainer {
 			const ssd = Math.round(sampleStandartDeviation(this.reactionTime))
 			const res = this.getAverageTime() + 2 * ssd
 
-			this.setLabel(`Поздравляем: ваш результат ${res} мс. ${this.chooseResult(this.results, res)} ssd: ${ssd}`)
-			this.startButton.disabled = false
+			this.setLabel(`Поздравляем: ваш результат ${res} мс. ${this.chooseResult(this.results, res)} ssd: ${ssd}, avg = ${this.getAverageTime()}`)
 			document.removeEventListener("keydown", this.listener)
 		}, time)
 	}
 }
-function openModalW() {
-	document.getElementById("modal").style.display = "block";
-}
 
-function closeModalW() {
-	document.getElementById("modal").style.display = "none";
-}
 
-window.onclick = function (event) {
-	const modal = document.getElementById("modal");
-	if (event.target === modal) {
-		modal.style.display = "none";
+
+
+class Test2 extends TestContainer {
+	constructor(squares, startButton, resultLabel, baseColor = "lightgrey", amount = 10) {
+		super(squares, startButton, resultLabel, baseColor, amount)
+		this.results = [
+			new Result(0, amount * 0.68, "У вас неудовлетворительная сложная реакция!"),
+			new Result(amount * 0.68, amount * 0.8, "У вас удовлитворительная сложная реакция!"),
+			new Result(amount * 0.8, amount * 0.94, "У вас хорошая сложная реакция!"),
+			new Result(amount * 0.94, amount + 1, "У вас отличная сложная реакция!"),
+		]
+
+		this.init()
+	}
+
+	init() {
+		this.startButton.onclick = () => {
+			this.setLabel("Тест начался!")
+			this.test()
+		}
+
+		this.listener = (event) => {
+			if (event.code !== "Space" && event.code !== "Enter") {
+				return;
+			}
+			this.clickHandler(event.code)
+		}
+	}
+
+	test() {
+		this.reset()
+
+		let time = 1000
+		this.order = []
+
+		for (let i = 0; i < this.amount; i++) {
+			time += getRandomInt(1000, 3000)
+			let color = i === 0 ? "yellow" : this.colors[getRandomInt(0, 3)]
+			this.step(color, time)
+			this.order.push(color)
+		}
+
+		document.addEventListener("keydown", this.listener)
+
+		this.end(time + 2000)
+	}
+
+	clickHandler(code) {
+		if (!this.checkCurrentSquare()) {
+			//this.addReactionTime(1000)
+			return
+		}
+
+		if (((this.num === 1 || this.order[this.num - 2] === this.order[this.num - 1]) && code === "Enter")
+			|| this.order[this.num - 2] !== this.order[this.num - 1] && code == "Space") {
+			this.correct++
+		} else {
+			this.incorrect++
+		}
+
+		this.updateTime()
+		this.changeCurrentSquareColor(this.baseColor)
+	}
+
+	step(color, time) {
+		setTimeout(() => {
+			if (this.checkCurrentSquare()) {
+				this.incorrect++
+			}
+			this.updateTime()
+			this.changeCurrentSquareColor(this.baseColor)
+			this.curSquare = (this.curSquare + 1) % 3
+			this.changeCurrentSquareColor(color)
+			this.num++
+			this.time = performance.now()
+		}, time)
+	}
+
+	updateTime() {
+		if (this.checkCurrentSquare()) {
+			this.addReactionTime(performance.now() - this.time)
+		}
+		this.setLabel(`Правильно: ${this.correct}. Неправильно: ${this.incorrect}`)
+	}
+
+	end(time) {
+		setTimeout(() => {
+			this.updateTime()
+			this.resetColors()
+
+			const res = this.chooseResult(this.results, this.correct)
+
+			this.setLabel(`Поздравляем: Среднее время: ${this.getAverageTime()} мс. Правильно: ${this.correct}. Неправильно: ${this.incorrect}. ${res}`)
+			document.removeEventListener("keydown", this.listener)
+		}, time)
 	}
 }
+
+
+class Test3 extends Test2 {
+	constructor(squares, startButton, resultLabel, baseColor = "lightgrey", amount = 70) {
+		super(squares, startButton, resultLabel, baseColor, amount)
+		this.results = [
+			new Result(0, amount * 0.68, "У вас неудовлетворительная сложная реакция!"),
+			new Result(amount * 0.68, amount * 0.8, "У вас удовлитворительная сложная реакция!"),
+			new Result(amount * 0.8, amount * 0.94, "У вас хорошая сложная реакция!"),
+			new Result(amount * 0.94, amount + 1, "У вас отличная сложная реакция!"),
+		]
+
+		this.init()
+	}
+
+
+	test() {
+		this.reset()
+
+		let time = 1000
+
+		let speed = 30
+
+		this.order = []
+
+		for (let i = 0; i < this.amount; i++) {
+			if ((i + 1) % 10 === 0) {
+				speed += 10
+			}
+			time += 60 / speed * 1000
+			let color = i === 0 ? "yellow" : this.colors[getRandomInt(0, 3)]
+			this.step(color, time)
+			this.order.push(color)
+		}
+
+		document.addEventListener("keydown", this.listener)
+
+		this.end(time + 2000)
+	}
+
+	end(time) {
+		setTimeout(() => {
+			this.updateTime()
+			this.resetColors()
+
+			const res = this.chooseResult(this.results, this.correct)
+
+			this.setLabel(`Поздравляем: Среднее время: ${this.getAverageTime()} мс. Правильно: ${this.correct}. Неправильно: ${this.incorrect}. ${res}`)
+			document.removeEventListener("keydown", this.listener)
+		}, time)
+	}
+}
+
+
+
+
+
+
+const testButtons = document.querySelectorAll(".test .btn")
+
+//блокировать нажатие на кнопку при нажатии пробела или enter
+document.addEventListener('keydown', (event) => {
+	console.log(event.code)
+	if ((event.code === 'Space' || event.code == "Enter") && isActive(testButtons)) {
+		event.preventDefault();
+	}
+});
+
+
+const t1 = new Test1(
+	document.querySelectorAll("#test1 .field .square"),
+	document.querySelector("#test1 .btn"),
+	document.querySelector("#test1 .result")
+)
+
+const t2 = new Test2(
+	document.querySelectorAll("#test2 .field .square"),
+	document.querySelector("#test2 .btn"),
+	document.querySelector("#test2 .result")
+)
+
+const t3 = new Test3(
+	document.querySelectorAll("#test3 .field .square"),
+	document.querySelector("#test3 .btn"),
+	document.querySelector("#test3 .result")
+)
+
